3a81506e4bafe499523ecabd8eae9f3e52260826
31daylee <31daylee@gmail.com>
2023-11-07 10:38:51 +0900

    Option API ... computed DONE

---
+++ ch03/src/components/OptionComponent3.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: "OptionComponent3",
+  computed: {
+    totPrice: function () {
+      return this.price * this.count;
+    },
+  },
+  data() {
+    return {
+      price: 1000,
+      count: 1,
+    };
+  },
+};
+</script>
+<template>
+  <h4>computed 옵션</h4>
+  <div>
+    <!--input 태그는 양방향 바인딩(v-model) 떠올리기 cf) 단방향 (v-bind)-->
+    <input type="number" v-model="price" />원
+    <input type="number" v-model="count" />개수
+    <p>금액 :{{ totPrice }}원</p>
+  </div>
+</template>
+<style scoped></style>
