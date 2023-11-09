ab162550db3c57d01bcaedcf9f10199de1ee404c
31daylee <31daylee@gmail.com>
2023-11-09 10:53:50 +0900

    Composition API DONE

---
+++ ch04/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+/*
+  날짜 : 2023/11/09
+  이름 : 이현정
+  내용 : ch04. Coposition API
+
+  Vue 프로젝트 생성 방식
+  - Options API 기반 : npm init vite-app 프로젝트명
+  - Composition API 기반 : npm init vite@latest 
+*/
+import { provide } from "vue";
+import Component1 from "./components/Component1.vue";
+import Component2 from "./components/Component2.vue";
+import Component3 from "./components/Component3.vue";
+
+export default {
+  name: "App",
+  components: { Component1, Component2, Component3 },
+  setup() {
+    const user1 = {
+      name: "김유신",
+      age: 23,
+      addr: "서울",
+    };
+    // App 컴포넌트 하위 컴포넌트에서 inject 주입
+    provide("user1", user1);
+  },
+};
+</script>
+<template>
+  <h3>ch04. Composition API</h3>
+
+  <h4>1) Reactivity</h4>
+  <Component1 />
+
+  <h4>2) LifeCycle Hooks</h4>
+  <Component2 />
+
+  <h4>3) Dependency Injection</h4>
+  <Component3></Component3>
+</template>
---
+++ ch04/src/components/Component3.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { provide } from "vue";
+import Component4 from "./Component4.vue";
+const user2 = {
+  name: "홍길동",
+  age: 32,
+  addr: "서울",
+};
+
+provide("user2", user2);
+</script>
+<template>
+  <h4>Component3</h4>
+  <Component4 />
+</template>
+<style scoped></style>
